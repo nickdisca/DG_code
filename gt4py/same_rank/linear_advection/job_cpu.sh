4e3c10b638e4ea7513160eb65b5e6c924b0664f7
Kalman Szenes <szenes.kalman@gmail.com>
2022-05-25 10:55:18 +0200

    ported rk2

--- gt4py/same_rank/linear_advection/job_cpu.sh
+++ gt4py/same_rank/linear_advection/job_cpu.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 
-#SBATCH -C mc -A csstaff -o output/sine/space_4_time_4.out -t 24:0:0
+#SBATCH -C mc -A csstaff -o output/sine/space_3_time_2.out -t 24:0:0
 
-rank=3
-runge=4
+rank=2
+runge=2
 
 srun python main.py 20 $rank $runge cpu
 srun python main.py 40 $rank $runge cpu
 srun python main.py 80 $rank $runge cpu
 srun python main.py 160 $rank $runge cpu
-srun python main.py 320 $rank $runge cpu
-srun python main.py 640 $rank $runge cpu
-srun python main.py 1280 $rank $runge cpu
-srun python main.py 2560 $rank $runge cpu
-srun python main.py 5120 $rank $runge cpu
+# srun python main.py 320 $rank $runge cpu
+# srun python main.py 640 $rank $runge cpu
+# srun python main.py 1280 $rank $runge cpu
+# srun python main.py 2560 $rank $runge cpu
+# srun python main.py 5120 $rank $runge cpu
