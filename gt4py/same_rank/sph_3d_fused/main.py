f334714a55536c9b1c2414af3f69aef24a34ef33
szeneskalman <szenes.kalman@gmail.com>
2022-06-15 10:39:09 +0200

    working sph3d

--- gt4py/same_rank/sph_3d_fused/main.py
+++ gt4py/same_rank/sph_3d_fused/main.py
@@ -132,7 +132,7 @@ if eq_type == 'swe' or eq_type == "swe_sphere":
     dt = courant * min((radius * np.sin(hx) * np.sin(hy), radius * np.sin(hx) * np.cos(hy))) / ((r+1) * alpha)
     # niter = int(T/dt)
     niter = 1000
-    plot_freq = 100000
+    plot_freq = 100
     # print(f'{dt = }')
     # quit()
 
--- gt4py/same_rank/sph_3d_fused/plotter.py
+++ gt4py/same_rank/sph_3d_fused/plotter.py
@@ -27,7 +27,7 @@ class Plotter():
         self.cbar = [None] * 3
 
 
-    def plot_solution(self,u,init=False, plot_type='contour', fname='final_step.png',title='',show=False):
+    def plot_solution(self,u,init=False, plot_type='contour', fname='',title='',show=False):
         self.fig.suptitle(title)
         z_component = 0
         x_u    = np.zeros(self.nx*self.r)
