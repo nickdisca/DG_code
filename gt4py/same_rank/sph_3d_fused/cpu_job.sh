58bfe0644bb467b030c978b8cfd817b8a263685d
Kalman Szenes <szenes.kalman@gmail.com>
2022-07-01 14:41:31 +0200

    modified job files

--- gt4py/same_rank/sph_3d_fused/cpu_job.sh
+++ gt4py/same_rank/sph_3d_fused/cpu_job.sh
@@ -1,7 +1,19 @@
 #!/bin/bash 
 
-#SBATCH -C mc -o output/cpu_ifirst.out -A csstaff -t 24:0:0 
+#SBATCH -C mc -A csstaff -t 24:0:0
 
-srun python main.py 100 40 3 4 gt:cpu_ifirst
+#SBATCH -J cpu_nx640
+#SBATCH -o output/cpu_nx640.out
+
+srun python main.py 640 1 3 4 gt:cpu_ifirst
+srun python main.py 640 2 3 4 gt:cpu_ifirst
+srun python main.py 640 3 3 4 gt:cpu_ifirst
+srun python main.py 640 4 3 4 gt:cpu_ifirst
+srun python main.py 640 8 3 4 gt:cpu_ifirst
+srun python main.py 640 16 3 4 gt:cpu_ifirst
+
+
+# srun python main.py 640 20 3 4 gt:cpu_ifirst
+# srun python main.py 300 20 3 4 gt:cpu_ifirst
 # srun scalene --profile-all --cli --output cpu.prof main.py
-# srun python -m cProfile -o cprofile_dace_cpu.prof main.py
+# srun python -m cProfile -o nx300_nz20_cpu.prof main.py 300 20 3 4 gt:cpu_ifirst
--- gt4py/same_rank/sph_3d_fused/gpu_job.sh
+++ gt4py/same_rank/sph_3d_fused/gpu_job.sh
@@ -1,7 +1,24 @@
 #!/bin/bash 
 
-#SBATCH -C gpu -o output/profile_dace_gpu.out -A csstaff -t 24:0:0 
+#SBATCH -C gpu -A csstaff -t 24:0:0
 
-# srun python main.py 100 40 3 4 dace:gpu
+
+#SBATCH -J gpu_nx640
+#SBATCH -o output/gpu_nx640.out
+
+srun python main.py 640 1 3 4 gt:gpu
+srun python main.py 640 2 3 4 gt:gpu
+srun python main.py 640 3 3 4 gt:gpu
+srun python main.py 640 4 3 4 gt:gpu
+srun python main.py 640 8 3 4 gt:gpu
+srun python main.py 640 16 3 4 gt:gpu
+
+# srun python main.py 10 1 3 4 gt:gpu
+# srun python main.py 20 1 3 4 gt:gpu
+# srun python main.py 40 1 3 4 gt:gpu
+# srun python main.py 80 1 3 4 gt:gpu
+# srun python main.py 160 1 3 4 gt:gpu
+# srun python main.py 320 1 3 4 gt:gpu
+# srun python main.py 640 1 3 4 gt:gpu
 # srun scalene --profile-all --cli --output cpu.prof main.py
-srun python -m cProfile -o dace_gpu_fused.prof main.py
+# srun python -m cProfile -o nx300_20_gpu.prof main.py 300 20 3 4 gt:gpu
