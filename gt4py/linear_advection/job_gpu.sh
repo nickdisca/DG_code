31c46b6ce26041cccacd8cb5797ee295eec153ed
szeneskalman <szenes.kalman@gmail.com>
2022-08-05 18:29:38 +0200

    removed job files

--- gt4py/linear_advection/job_gpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-
-#SBATCH -C gpu -A csstaff -o order_3_gpu.out -t 24:0:0
-
-srun python main.py 1280 2 3 cuda
-srun python main.py 2560 2 3 cuda
-srun python main.py 5120 2 3 cuda
-srun python main.py 10240 2 3 cuda
--- gt4py/swe_3d/job.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-#SBATCH -C gpu -A csstaff -t 24:0:0 -o output/gpu.out
-
-srun python main.py 100 20 3 4 cuda
